fac9525ad4a8d467facfc043201802641b5b694c
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2023-05-09 15:59:36 +0800

    Fix some UI + added header for game config

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1>Boole Bots Arena</h1>
+      </header>
+      <main>
+        <div className="game-config">
+          <h1>Game Config</h1>
+          <h2>Apply to all bots:</h2>
+        </div>
+        <div>
+          <p>Arena</p>
+        </div>
+        <div>Leaderboard</div>
+      </main>
+      {/* reserved for footer */}
+      {/* <footer>
+        <div>Game Config</div>
+        <div>Boole Arena</div>
+        <div>Leader boards</div>
+      </footer> 
+      */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
