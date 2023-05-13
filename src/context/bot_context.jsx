bd3febffb4435e7ab446f1c9c9af450820da5c7b
Emerson Bualat <96451055+emerson-bualat@users.noreply.github.com>
2023-05-14 06:43:08 +0800

    Add initialState to bot_context

--- src/context/bot_context.jsx
+++ src/context/bot_context.jsx
@@ -1,5 +1,14 @@
 import { useContext, useReducer, useEffect, createContext } from "react";
 
+const initialState = {
+  bots: [],
+  boolean_operation: "AND",
+  speed: 0,
+  isBattleStarted: false,
+  isBattleStopped: false,
+  animationFrame: null,
+};
+
 const BotContext = createContext();
 
 export const BotsProvider = ({ children }) => {
