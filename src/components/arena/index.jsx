ffa28947fceb8faa83ec634388e8474902652aa0
Emerson Bualat <96451055+emerson-bualat@users.noreply.github.com>
2023-05-14 05:54:47 +0800

    Add starter files for arena component

---
+++ src/components/arena/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./arena.css";
+
+const index = () => {
+  return (
+    <>
+      <section className="arena">
+        <h1>arena</h1>
+      </section>
+    </>
+  );
+};
+
+export default index;
