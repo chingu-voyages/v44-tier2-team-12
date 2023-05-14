9f64f9bb2c7a87dc40e849b05464051d2ae15496
Emerson Bualat <96451055+emerson-bualat@users.noreply.github.com>
2023-05-14 14:02:24 +0800

    Add checkCollision to check collision between bot1 & bo2

--- src/utils/helpers.jsx
+++ src/utils/helpers.jsx
@@ -81,3 +81,76 @@ export const checkResult = (operator, bot1, bot2) => {
     }
   }
 };
+
+export const getDistance = (xPosition1, yPosition1, xPosition2, yPosition2) => {
+  var result = Math.sqrt(
+    Math.pow(xPosition2 - xPosition1, 2) + Math.pow(yPosition2 - yPosition1, 2)
+  );
+  return result;
+};
+
+export const checkCollision = (
+  bot1,
+  bot2,
+  bots,
+  animationFrame,
+  animateBots
+) => {
+  if (
+    getDistance(
+      bot1.xPosition,
+      bot1.yPosition,
+      bot2.xPosition,
+      bot2.yPosition
+    ) >=
+    bot1.radius + bot2.radius
+  ) {
+    bot1.invincible = false;
+    bot2.invincible = false;
+  }
+
+  if (bot1.invincible === true && bot2.invincible === true) {
+    return;
+  }
+
+  if (
+    getDistance(
+      bot1.xPosition,
+      bot1.yPosition,
+      bot2.xPosition,
+      bot2.yPosition
+    ) <
+    bot1.radius + bot2.radius
+  ) {
+    /* TODO: when collision occurs you must save the previous direction so that when it win you will know it's previous direction */
+    const bot1PrevDirection = bot1.direction;
+    const bot2PrevDirection = bot2.direction;
+
+    const result = checkResult("AND", bot1.booleanValue, bot2.booleanValue);
+
+    // wait for 1second(1000) before checking results
+    cancelAnimationFrame(animationFrame);
+    setTimeout(() => {
+      requestAnimationFrame(animateBots);
+    }, 3000);
+
+    if (result == "Tie") {
+      bot1.dx = 5; // 5 should be changed to speed variable
+      bot1.dy = 5;
+      bot1.direction = bot1PrevDirection;
+
+      bot2.dx = 5;
+      bot2.dy = 5;
+      bot2.direction = bot2PrevDirection;
+      bot1.invincible = true;
+      bot2.invincible = true;
+
+      console.log(`${bot1.text} vs ${bot2.text} it's a TIE!`);
+    } else {
+      const randomNumber = Math.round(Math.random());
+      let newBots = bots.filter((_, index) => index != randomNumber);
+      console.log(newBots[0].text + " is the winner!");
+      bots.splice(0, bots.length, ...newBots);
+    }
+  }
+};
